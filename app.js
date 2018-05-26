b815b61fee7ecb82f16e2a8979c5b98deb458220
Jakub <jakub.rozi.rozak@gmail.com>
2018-05-26 09:03:23 +0100

    first version

--- app.js
+++ app.js
@@ -15,8 +15,29 @@ var quotes = [{
         }]
 
 
+
+
+
+
 var random = Math.floor(Math.random()*(quotes.length))
-console.log(random)
-var chosen = document.createElement('h1')
-chosen.textContent = quotes[random].q
-generate.appendChild(chosen)
+
+var randomQuote = quotes[random].q
+var randomPerson = quotes[random].person
+
+//console.log(randomQuote)
+//console.log(randomPerson)
+
+var chosenPerson = document.createElement('h1')
+chosenPerson.className = 'quote'
+chosenPerson.textContent = randomQuote
+
+var chosenQuote = document.createElement('h2')
+chosenQuote.textContent = randomPerson
+chosenQuote.className = 'person'
+
+
+generate.appendChild(chosenPerson)
+generate.appendChild(chosenQuote)
+
+
+
